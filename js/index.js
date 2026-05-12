99ed00af09c54cd9a68747e893ea589c2d806fa0
Felipe-pixel12 <42021@raphaeldisanto.com.br>
2026-05-12 08:05:57 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+//Lista de melhores memes com nome e imagem local
+const MelhoresMEMES =[
+    { nome: "Meme-1", imagem: "img/meme-1.jpg"}
+    { nome: "Meme-2", imagem: "img/meme-2.jpg"},
+    { nome: "Meme-3", imagem: "img/meme-3.jpg"},
+    { nome: "Meme-4", imagem: "img/meme-4.jpg"},
+    { nome: "Meme-5", imagem: "img/meme-5.jpg"},
+    { nome: "Meme-6", imagem: "img/meme-6.jpg"},
+    { nome: "Meme-7", imagem: "img/meme-7.jpg"},
+    { nome: "Meme-8", imagem: "img/meme-8.jpg"},
+]
+
+//Começa todos os votos zerados
+const votos = new Array(MelhoresMEMES.length).fill(0);
+
+
